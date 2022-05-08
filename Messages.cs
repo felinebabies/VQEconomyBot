46541531a183b1d157f3efda6df3a8bc1aa7cc47
Syuji <commentonly.is@gmail.com>
2022-05-08 19:06:21 +0900

    残高確認コマンドを仮に実装した。

--- Messages.cs
+++ Messages.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace VestalisQuintet.EconomyBot
 {
@@ -16,6 +18,19 @@ namespace VestalisQuintet.EconomyBot
         public async Task helloVQ()
         {
             string Messages = "めたんもそう思います\n\n";
+
+            await ReplyAsync(Messages);
+        }
+
+        /// <summary>
+        /// 残高を確認する
+        /// </summary>
+        /// <returns></returns>
+        [Command("balance")]
+        public async Task balance(IUser user = null)
+        {
+		    var userInfo = user ?? Context.Client.CurrentUser;
+            string Messages = userInfo.Username + "さんの残高は99999アドです。\n\n";
 
             await ReplyAsync(Messages);
         }
